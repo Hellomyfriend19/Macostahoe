8fcf6138cba78dc1d2435cac2a2864e73420f5bf
Ioane Imnadze <100489172+Hellomyfriend19@users.noreply.github.com>
2026-06-10 22:28:54 +0400

    Big Update

--- info2.ts
+++
@@ -1,5 +0,0 @@
-(async () => {
-    let url = "https://wsrv.nl/?url=" + encodeURIComponent("https://512pixels.net/wp-content/uploads/2025/06/26-Tahoe-Light-6K-thumb.jpeg");
-    let res = await fetch(url, { method: 'HEAD' });
-    console.log(res.status, [...res.headers.entries()]);
-})();
--- test3.js
+++
@@ -1,5 +0,0 @@
-const r = /url\((.*)\)/i;
-const s1 = 'url("https://github.com/user/repo/New Project (13).png")';
-const s2 = 'url(https://github.com/user/repo/New Project (13).png)';
-console.log("s1:", s1.match(r)[1].replace(/^['"]|['"]$/g, ''));
-console.log("s2:", s2.match(r)[1].replace(/^['"]|['"]$/g, ''));
